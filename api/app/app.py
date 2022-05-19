0bae4d5e1f5d5ce005405f58f7b6d83f538024e0
Andrew Minkin <minkin.andrew@gmail.com>
2022-05-19 18:37:17 +0600

    Added SQLAlchemy

--- api/app/app.py
+++ api/app/app.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from app.extensions import db
 
 def create_app(testing=False):
+    """Application factory, used to create application."""
     app = Flask(__name__)
     app.config.from_object("config.settings")
+    if testing:
+        app.config["TESTING"] = True
+
+    configure_extensions(app)
+
     return app
 
+def configure_extensions(app):
+    """Configure flask extensions."""
+
+    db.init_app(app)
