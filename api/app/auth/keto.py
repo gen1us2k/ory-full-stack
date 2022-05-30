2976bab9a80917c72cd1c0bdf07c11ec95cea866
Andrew Minkin <minkin.andrew@gmail.com>
2022-05-30 18:09:11 +0600

    Added keto client

--- api/app/auth/keto.py
+++ api/app/auth/keto.py
@@ -2,11 +2,33 @@ import requests
 
 
 class AccessControl:
-    def __init__(self):
-        pass
+    def __init__(self, keto_write_url, keto_read_url):
+        self.keto_write_url = keto_write_url
+        self.keto_read_url = keto_read_url
 
-    def is_allowed(self):
-        pass
+    def is_allowed(self, namespace, obj, relation, subject_id):
+        return self.check_permissions(namespace, obj, relation, subject_id)
 
-    def check_permissions(self):
-        pass
+    def check_permissions(self, namespace, obj, relation, subject_id) -> bool:
+        r = requests.get(
+            f"{self.keto_read_url}/relation-tuples/check",
+            data={
+                "object": obj,
+                "relation": relation,
+                "subject_id": subject_id,
+                "namespace": namespace,
+            }
+        )
+        data = r.json()
+        return data.get('allowed')
+
+    def add_permissions(self, namespace, obj, relation, subject_id):
+        r = requests.put(
+            f"{self.keto_write_url}/relation-tuples",
+            data={
+                "object": obj,
+                "relation": relation,
+                "subject_id": subject_id,
+                "namespace": namespace
+            }
+        )
