6a0eeb0da95691dd4b6177ad7c1630f00676700a
Andrew Minkin <minkin.andrew@gmail.com>
2022-05-30 17:39:24 +0600

    Added roles

--- api/app/models/user.py
+++ api/app/models/user.py
@@ -1,11 +1,20 @@
 from app.extensions import db
 from app.models.base import PkModel
+import enum
+
+
+class UserRole(enum.Enum):
+    USER = "user"
+    MODERATOR = "moderator"
+    ADMIN = "admin"
 
 
 class User(PkModel):
     __tablename__ = 'users'
     kratos_id = db.Column(db.String(36))
     email = db.Column(db.String())
+
+    role = db.Column(db.Enum(UserRole), default=UserRole.USER)
 
     threads = db.relationship('Thread', backref='user', lazy='dynamic')
     comments = db.relationship('Comment', backref='user', lazy='dynamic')
